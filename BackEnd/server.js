88799afe05249d7af166ed9cfc4b1a656dca9f35
Sandun Herath <sandunherath124@gmail.com>
2021-08-23 22:42:10 +0530

    order controller and routes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,31 +1,37 @@
-import express from "express";
-import dotenv from "dotenv";
-import colors from "colors";
-import bodyParser from "body-parser";
-import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-dotenv.config();
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
-connectDB();
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
-const app = express();
-app.use(bodyParser.json());
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+dotenv.config()
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use(notFound);
-app.use(errorHandler);
+connectDB()
 
-const PORT = process.env.PORT || 5000;
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API is running....')
+})
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-);
+)
